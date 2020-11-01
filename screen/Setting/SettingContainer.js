4ea94aa8dd43f1f4d51bfe821ec89d97c10afaf3
jsjphone8 <jsjphone8@gmail.com>
2020-11-01 21:22:24 +0900

    초기화 버튼 기능 구현 완료

--- screen/Setting/SettingContainer.js
+++ screen/Setting/SettingContainer.js
@@ -1,15 +1,22 @@
 import React from "react";
 import SettingPresenter from "./SettingPresenter";
 import AsyncStorage from "@react-native-community/async-storage";
-import RNRestart from "react-native-restart";
-
+import CreateTwoButtonAlert from "../../component/CreateTwoButtonAlert";
 export default ({ navigation, route }) => {
-  const resetClick = async () => {
+  const confirmOnPress = async () => {
     await AsyncStorage.removeItem("StudentId");
     await AsyncStorage.removeItem("ClassList");
-    console.log("리스타트 클릭");
-    // console.log(typeof RNRestart);
-    RNRestart.Restart();
+  };
+  const denyOnPress = () => {};
+  const resetClick = () => {
+    CreateTwoButtonAlert({
+      title: "학번, 수업목록 초기화",
+      message: "초기화 하시겠습니까?",
+      confirmMessage: "좋아요",
+      denyMessage: "싫어요",
+      confirmOnPress,
+      denyOnPress,
+    });
   };
 
   return <SettingPresenter navigation={navigation} resetClick={resetClick} />;
