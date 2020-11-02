08bbba9295f95fd3fd2ed75f2fb66c42853dcf7f
jsjphone8 <jsjphone8@gmail.com>
2020-11-02 15:03:10 +0900

    주석 및 공백 삭제

--- screen/Setting/SettingContainer.js
+++ screen/Setting/SettingContainer.js
@@ -2,10 +2,16 @@ import React from "react";
 import SettingPresenter from "./SettingPresenter";
 import AsyncStorage from "@react-native-community/async-storage";
 import CreateTwoButtonAlert from "../../component/CreateTwoButtonAlert";
+// import RNRestart from "react-native-restart";
+
 export default ({ navigation, route }) => {
   const confirmOnPress = async () => {
     await AsyncStorage.removeItem("StudentId");
     await AsyncStorage.removeItem("ClassList");
+    // console.log("버튼 눌리기");
+    // console.log("타입: " + typeof RNRestart);
+    // console.log("그냥찍기: " + RNRestart);
+    // RNRestart.Restart();
   };
   const denyOnPress = () => {};
   const resetClick = () => {
